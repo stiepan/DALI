7b6309dc7921b48fac8948fb9d81e3a53f64c846
Kamil Tokarski <ktokarski@nvidia.com>
2023-06-26 12:00:45 +0200

    Wrap pipeline into torch iterator
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- docs/examples/use_cases/pytorch/vit/vit.py
+++ docs/examples/use_cases/pytorch/vit/vit.py
@@ -3,17 +3,23 @@ from mixup_cutmix import mixup_cutmix, mixup_cutmix_one_hot_encoding
 from random_erasure import random_erasure
 
 from nvidia.dali import fn, pipeline_def
+from nvidia.dali.plugin.pytorch import DALIClassificationIterator
+
+import torch
 
 img_shape = (224, 224)
 random_area = (0.08, 1.0)
 random_aspect_ratio = (0.75, 1.3333)
 data_path = "/home/ktokarski/DALI_extra/db/single/jpeg"
 num_classes = 20
+batch_size = 16
+device_id = 0
+num_threads = 4
 
 
-def create_pipeline(img_shape=img_shape, random_area=random_area,
+def create_pipeline(rank=0, world_size=1, img_shape=img_shape, random_area=random_area,
                     random_aspect_ratio=random_aspect_ratio, num_classes=num_classes,
-                    data_path=data_path, batch_size=16, mixup_alpha=0.8, cutmix_alpha=1.,
+                    data_path=data_path, batch_size=batch_size, mixup_alpha=0.8, cutmix_alpha=1.,
                     mixup_prob=1., mixup_switch_prob=0.5, mixup_cutmix_mode="elem",
                     label_smoothing=0.1, re_count=1, re_prob=0.25, color_range=255,
                     re_mode="random", ra_n=2, ra_m=9, ra_num_bins=11, ra_prob=0.5, ra_std=0.5,
@@ -27,9 +33,11 @@ def create_pipeline(img_shape=img_shape, random_area=random_area,
     use_mixup_cutmix = mixup_alpha > 0 or cutmix_alpha > 0
     use_random_erasure = re_prob > 0
 
-    @pipeline_def(enable_conditionals=True, batch_size=batch_size, device_id=0, num_threads=4)
+    @pipeline_def(enable_conditionals=True, batch_size=batch_size, device_id=device_id,
+                  num_threads=num_threads)
     def pipeline():
-        image, label = fn.readers.file(file_root=data_path, random_shuffle=True)
+        image, label = fn.readers.file(name="Reader", shard_id=rank, num_shards=world_size,
+                                       file_root=data_path, random_shuffle=True)
         image = fn.decoders.image(image, device="mixed")
         image = fn.random_resized_crop(image, size=img_shape, random_area=random_area,
                                        random_aspect_ratio=random_aspect_ratio)
@@ -45,11 +53,24 @@ def create_pipeline(img_shape=img_shape, random_area=random_area,
         if use_random_erasure:
             image = random_erasure(image, prob=re_prob, mode=re_mode, count=re_count,
                                    color_range=color_range)
-        return image, label
+        return image, label.gpu()
 
     return pipeline()
 
 
-p = create_pipeline()
-p.build()
-images, labels = p.run()
+if torch.distributed.is_initialized():
+    rank = torch.distributed.get_rank()
+    world_size = torch.distributed.get_world_size()
+else:
+    rank = 0
+    world_size = 1
+
+pipe = create_pipeline(rank=rank, world_size=world_size)
+pipe.build()
+train_loader = DALIClassificationIterator(pipe, reader_name="Reader", fill_last_batch=False)
+
+for i, (pipe_out, ) in enumerate(train_loader):
+    assert isinstance(pipe_out["data"], torch.Tensor)
+    assert isinstance(pipe_out["label"], torch.Tensor)
+    print(pipe_out["data"].device, pipe_out["label"].device)
+    break
